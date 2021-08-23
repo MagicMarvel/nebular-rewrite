56c3718917cbe5ddd70ad86347afed1303fb47dc
MagicMarvel <magicmarvel@163.com>
2021-08-23 21:08:24 +0800

    添加了需要的大部分包

---
+++ src/pages/home/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+class index extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+          <div className="flex-shrink-0">
+            {" "}
+            <img class="h-12 w-12" src="src\logo.svg" alt="ChitChat Logo" />
+          </div>
+          Home
+        </div>
+      </div>
+    );
+  }
+}
+export default index;
