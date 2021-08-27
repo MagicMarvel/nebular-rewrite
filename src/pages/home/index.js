799e0ce7d8246130c51c4c8ad9a9d45dc00be4c0
MagicMarvel <magicmarvel@163.com>
2021-08-27 20:10:13 +0800

    安装tailwind css

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,15 +1,15 @@
 import React from "react";
+import styled from "styled-components";
+
+const HelloWorld = styled.div`
+  background-color: lavenderblush;
+`;
+
 class index extends React.Component {
   render() {
     return (
       <div>
-        <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
-          <div className="flex-shrink-0">
-            {" "}
-            <img class="h-12 w-12" src="src\logo.svg" alt="ChitChat Logo" />
-          </div>
-          Home
-        </div>
+        <div className="bg-yellow-600 ">hello world</div>
       </div>
     );
   }
